7fa0b36a630fa5ce752757014d6e590d6d8aef51
Lorenzo Widmann <lorenzo.widmann@gmail.com>
2022-11-10 20:13:02 +0100

    first commit

---
+++ AutoSearcherFunction.py
@@ -0,0 +1,168 @@
+from operator import sub
+import requests
+from bs4 import BeautifulSoup
+
+#Parole da rimovere per ogni ricerca
+word_remove_list = ['scheda', 'ricambi', 'cover', 'protezione', 'rotto', 'batteria', 'custodia']
+
+def average(list):
+    if len(list) == 0:
+        avg = 0
+    else:
+        avg = sum(list)/len(list)
+    return avg
+
+def out_average(list):
+    outList = []
+    averageValue = average(list)
+    for x in list: 
+        if x > averageValue/2: 
+            if x < averageValue*2:
+                outList.append(x)
+    return outList
+
+def is_float(list):
+    floatList = []
+    for x in list:
+        xIsFloat = False
+        try:
+            float(x)
+            xIsFloat = True
+        except ValueError:
+            xIsFloat = False
+
+        if xIsFloat == True: 
+            floatList.append(float(x))
+    return floatList
+
+def page_request_result(site_url, id_string):
+    request_result=requests.get(site_url)
+    soup = BeautifulSoup(request_result.text, "html.parser")
+    page_top=soup.find(id=id_string)
+    return page_top
+
+def gen_word_remover(list):
+    single_string = ''
+    for x in list: 
+        if list.index(x) != 0:
+            single_string = single_string + '+' + x
+        else: 
+            single_string = x
+    return single_string
+
+def link_current_items(search, search_remove, size, list = word_remove_list):
+    if search_remove != '':
+        complete_searche_remove = search_remove + '+' + size_differential(size) + '+' + gen_word_remover(list)
+    else:
+        complete_searche_remove = size_differential(size) + '+' + gen_word_remover(list)
+    complete_searche = search + '+' + size
+    site_url = f'https://www.ebay.it/sch/i.html?_from=R40&_nkw={complete_searche}&_in_kw=3&_ex_kw={complete_searche_remove}&_sacat=0&_udlo=&_udhi=&LH_Auction=1&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15&_stpos=39100&_sargn=-1%26saslc%3D1&_salic=101&_sop=1&_dmd=1&_ipg=60'
+    return site_url
+
+def link_solded_items(search, search_remove, size, list = word_remove_list):
+    if search_remove != '':
+        complete_searche_remove = search_remove + '+' + size_differential(size) + '+' + gen_word_remover(list)
+    else:
+        complete_searche_remove = size_differential(size) + '+' + gen_word_remover(list)
+    complete_searche = search + '+' + size
+    site_url = f'https://www.ebay.it/sch/i.html?_from=R40&_nkw={complete_searche}&_in_kw=3&_ex_kw={complete_searche_remove}&_sacat=0&_udlo=&_udhi=&LH_Auction=1&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15&_stpos=39100&_sargn=-1%26saslc%3D1&_salic=101&_sop=1&_dmd=1&_ipg=60&LH_Sold=1&rt=nc'
+    return site_url
+
+def telegram_message(message):
+    token = "5222921867:AAFzL-IV4o1CU8C2Ncgea3yQO3VRREtGfwo"
+    chat_id = '236543289'
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+    requests.get(url).json()
+
+def items_price_average(url):
+    page_top = page_request_result(url,"mainContent")
+    if page_top is not None:
+        #finding the cost
+        phone_price = page_top.find_all(class_="s-item__price")
+        phone_shipping = page_top.find_all(class_="s-item__shipping s-item__logisticsCost")
+    
+        prices = []
+        shipping = []
+        i = 0 
+        for info in phone_price:
+            if len(phone_shipping) > i:
+                if len(phone_price) > i:
+                    if phone_shipping[i].getText() != 'Spedizione non specificata':
+                        if str(phone_price[i]).find('ITALIC') == -1:
+                                prices.append(info.getText().replace('EUR ', '').replace('.','').replace(',','.'))
+            i = i + 1 
+
+        i = 0
+        for info in phone_shipping:
+            if len(phone_shipping) > i:
+                if len(phone_price) > i:
+                    if phone_shipping[i].getText() != 'Spedizione non specificata':
+                        if str(phone_price[i]).find('ITALIC') == -1:
+                            shipping.append(info.getText().replace('+EUR ', '').replace(',','.').replace(' di spedizione', '').replace('Spedizione gratis', '0').replace('Spedizione non specificata','0'))
+            i = i + 1
+        
+        average_price = average(out_average(is_float(prices)))
+        average_shipping = average(out_average(is_float(shipping)))
+        return average_price + average_shipping
+
+    else: 
+        #returning fake output
+        fake_price = 0.00
+        return fake_price
+
+def timer_trigger(url, url_solded, trigger_timer):
+    page_top = page_request_result(url, "srp-river-results")
+    #Making sure that the page is correctly loaded 
+    if page_top is not None:
+        timer_asta = page_top.find_all(class_='s-item__time-left')
+        phone_name = page_top.find_all(class_='s-item__title')
+        phone_link = page_top.find_all(class_='s-item__link', href=True)
+        phone_price = page_top.find_all(class_='s-item__price')
+        phone_shipping = page_top.find_all(class_='s-item__shipping s-item__logisticsCost')
+        phone_condition = page_top.find_all(class_='SECONDARY_INFO')
+        for info in timer_asta:
+            #trigger condition
+            if info.getText()[0:2] == trigger_timer:
+                if phone_shipping[timer_asta.index(info)].getText().find(' spedizione stimata') == -1:
+
+                    if phone_condition[timer_asta.index(info)].getText() != 'Solo ricambi':
+
+                        if str(phone_price[timer_asta.index(info)]).find('ITALIC') == -1:
+
+                            if phone_price[timer_asta.index(info)].getText()[0:3] == 'EUR':
+                                
+                                if phone_shipping[timer_asta.index(info)].getText() != 'Spedizione non specificata':
+                                    float_phone_price = float(phone_price[timer_asta.index(info)].getText().replace('EUR ', '').replace('.','').replace(',','.'))
+                                    float_phone_shipping = float(phone_shipping[timer_asta.index(info)].getText().replace('+EUR ', '').replace('.','').replace(',','.').replace(' di spedizione', '').replace('Spedizione gratis', '0').replace(' spedizione stimata','').replace('Spedizione non specificata','0'))
+                                    
+                                    if (float_phone_price + float_phone_shipping) <= items_price_average(url_solded):
+                                        #Minimum ROI percentage (10%)
+                                        if (items_price_average(url_solded) / (float_phone_price + float_phone_shipping)) >= 1.1:
+                                        
+                                            if (float_phone_price + float_phone_shipping) >= (items_price_average(url_solded)/4):
+                                                telegram_message(
+                                                    phone_name[timer_asta.index(info)].getText() +'\n'+
+                                                    '\n'+ 
+                                                    info.getText() +'\n'+
+                                                    'CONDIZIONE: '+phone_condition[timer_asta.index(info)].getText() +'\n'+
+                                                    'PREZZO: '+phone_price[timer_asta.index(info)].getText() +'\n'+
+                                                    'SPEDIZIONE: '+phone_shipping[timer_asta.index(info)].getText().replace('di spedizione','').replace(' spedizione stimata','').replace('Spedizione non specificata','0') +'\n'+
+                                                    'PREZZO MEDIO: EUR '+str(f'{items_price_average(url_solded):.2f}').replace('.',',') +'\n'+
+                                                    phone_link[timer_asta.index(info)]['href'])
+
+#Size number 6
+def size_differential(size):
+    size_removal = ''
+    if size == '32GB':
+        size_removal = '64GB+128GB+256GB+512GB+1TB'
+    elif size == '64GB':
+        size_removal = '32GB+128GB+256GB+512GB+1TB'
+    elif size == '128GB':
+        size_removal = '32GB+64GB+256GB+512GB+1TB'
+    elif size == '256GB':
+        size_removal = '32GB+64GB+128GB+512GB+1TB'
+    elif size == '512GB':
+        size_removal = '32GB+64GB+128GB+256GB+1TB'
+    return size_removal
+
+#print(items_price_average('https://www.ebay.it/sch/i.html?_from=R40&_nkw=iPhone+12+128GB&_in_kw=3&_ex_kw=mini+pro+max+32GB+64GB+256GB+512GB+1TB+scheda+ricambi+cover+protezione+rotto+batteria+custodia&_sacat=0&_udlo=&_udhi=&LH_Auction=1&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15&_stpos=39100&_sargn=-1%%26saslc%%3D1&_salic=101&_sop=1&_dmd=1&_ipg=60&LH_Sold=1&rt=nc'))
---
+++ main.py
@@ -0,0 +1,152 @@
+from datetime import datetime
+from platform import platform
+import AutoSearcherFunction
+import time 
+import schedule
+from datetime import datetime 
+import os 
+import configparser
+import pytz
+import sys
+from termcolor import colored
+
+print('Version 0.2.6')
+
+path = ''
+if sys.platform.startswith('linux'):
+    #linux
+    path = '/home/ubuntu/AutoSearcher'
+if sys.platform == 'win32':
+    #windows
+    path = 'C:\\Users\\loren\\Desktop\\AutoSearcher'
+
+def current_time():
+    now = datetime.now(pytz.timezone('Europe/Rome'))
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+searching_items = []
+config = configparser.ConfigParser()
+#Tryng the path
+try:
+    config.read(os.path.join(path,'PhoneLinkConfiguration.ini'))
+    ITEMS = {}
+    for key in config['ITEMS']:
+        value = config.get("ITEMS", key)
+        if key[0:3] == 'typ':
+            ITEMS[key] = value 
+            searching_items.append(value)
+        if key[0:3] == 'cur':
+            ITEMS[key] = value.replace('%','%%') 
+        if key[0:3] == 'sol':
+            ITEMS[key] = value.replace('%','%%') 
+except: 
+    print(colored('Edit the path', 'red'))
+    sys.exit()
+
+#Current search
+risposta_ricerche = input('Ricerche in corso: (y)').lower()
+if risposta_ricerche == 'y':
+    for i in searching_items:
+        print(i.replace('+',' '))
+
+search_int = input('Vuoi inserire altri oggetti: (y)').lower()
+if search_int == 'y':
+    print('Procedi con inserire la ricerca che vuoi efettuare')
+    i = True 
+    while i == True:
+        items_name = input("Nome dell'oggetto: ").replace(' ','+')
+        #finding if the items already exist 
+        items_already_exist = False
+        for x in config['ITEMS']:
+            if x.lower().find(items_name.lower()) != -1: 
+                print(colored('Ricerca già inserita', 'red'))
+                items_already_exist = True
+                break
+        if items_already_exist == False:
+            search_remove = input('Parole da rimuovere: ').replace(' ','+')
+            size_question = input('Vuoi differenziare la memoria: (y)')
+            if size_question == 'y':
+                x = 1
+                while x <= 5:
+                    if x == 1: 
+                        size = '32GB'
+                    elif x == 2: 
+                        size = '64GB'
+                    elif x == 3: 
+                        size = '128GB'
+                    elif x == 4: 
+                        size = '256GB'
+                    elif x == 5:
+                        size = '512GB'
+                        
+                    #setting up links and name
+                    ITEMS['type_'+items_name+'_'+size] = items_name + '+' + size
+                    ITEMS['current_link_'+items_name+'_'+size] = AutoSearcherFunction.link_current_items(items_name, search_remove, size).replace('%','%%')
+                    ITEMS['solded_link_'+items_name+'_'+size] = AutoSearcherFunction.link_solded_items(items_name, search_remove, size).replace('%','%%')
+                    x = x + 1 
+
+            else:
+                #setting up links and name 
+                ITEMS['type_'+items_name] = items_name
+                ITEMS['current_link_'+items_name] = AutoSearcherFunction.link_current_items(items_name, search_remove, size='').replace('%','%%')
+                ITEMS['solded_link_'+items_name] = AutoSearcherFunction.link_solded_items(items_name, search_remove, size='').replace('%','%%')
+
+            risposta = ''
+            while risposta != 'y' or risposta!= 'n':
+                risposta = input('Vuoi inserire un altro oggetto? (y/n): ').lower()
+                if risposta == 'n':
+                    i = False
+                    break
+                if risposta == 'y':
+                    print('Procedi con inserire i dati richiesti')
+                    break
+                else:
+                    print('Rispondi solamente y o n')
+    
+    config['ITEMS'] = ITEMS
+    #Saving items
+    with open(os.path.join(path,'PhoneLinkConfiguration.ini'), 'w') as phone_link:
+        config.write(phone_link)
+    print(colored('CONFIGURATION DONE','green'))
+
+#Starting the search
+risposta_starter = input('Vuoi iniziare la ricerca: (y)')
+if risposta_starter == 'y':
+    #converting dictionary to array 
+    phone_type_array = []
+    selling_phone_array = []
+    solded_phone_array = [] 
+    for x in config['ITEMS']:
+        value = config.get("ITEMS", x)
+        if x[0:3] == 'typ':
+            phone_type_array.append(value)
+        if x[0:3] == 'cur':
+            selling_phone_array.append(value.replace('%%','%'))
+        if x[0:3] == 'sol':
+            solded_phone_array.append(value.replace('%%','%'))
+
+    def search_phone(): 
+        print('Searching...')   
+        for x in phone_type_array:
+            item_position = phone_type_array.index(x)
+            AutoSearcherFunction.timer_trigger(
+                selling_phone_array[item_position],
+                solded_phone_array[item_position],
+                '2h'
+            )
+            time.sleep(15)
+        AutoSearcherFunction.telegram_message('Ciclo andato a buon fine '+str(current_time()))
+        print('Ciclo andato a buon fine '+str(current_time()))
+
+    AutoSearcherFunction.telegram_message('Ricerca iniziata alle: '+str(current_time()))
+    print('Searching...')
+    schedule.every(1).hours.do(search_phone)
+    i = True
+    while i:
+        try:
+            schedule.run_pending()
+            time.sleep(1800)
+        except Exception as e:
+            AutoSearcherFunction.telegram_message('ERROR: '+str(e))
+            i = False
